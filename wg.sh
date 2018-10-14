dbf78bd0e61cc60b60a54bba3f492d001d885f17
Evgeny Cherkashin <eugeneai@irnok.net>
2018-10-14 09:31:19 +0800

    Restrict wg.sh import to root

--- wg.sh
+++ wg.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -x
-
-. /etc/wireguard/ip.conf
-
 if [[ $EUID -ne 0 ]]; then
     echo "Rerun as root" 1>&2
     sudo $0
@@ -11,6 +7,12 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Normal run as root"
+
+set -x
+
+. /etc/wireguard/ip.conf
+
+ip link del dev wg0 || true
 
 ip link add dev wg0 type wireguard
 ip address add dev wg0 $ME peer $OTHER
