2804cce31a2d3f7445c731cb429c969d8b8900eb
Evgeny Cherkashin <eugeneai@irnok.net>
2018-10-14 09:26:04 +0800

    Added a wg up script.

---
+++ wg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+. ~eugeneai/ip.sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Rerun as root" 1>&2
+    sudo $0
+    exit 0
+fi
+
+echo "Normal run as root"
+
+ip link add dev wg0 type wireguard
+ip address add dev wg0 $ME peer $OTHER
+wg setconf wg0 /etc/wireguard/wg.conf
+ip link set up dev wg0
+
